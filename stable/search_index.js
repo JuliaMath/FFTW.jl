860e83b41a859297ae8af3d427e45fe121e85cab
autodocs <autodocs>
2017-12-20 20:28:56 +0000

    build based on 8302e87

--- stable/search_index.js
+++ stable/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Installation",
     "category": "section",
-    "text": "The package is available for Julia versions 0.6 and up. To install it, runPkg.add(\"FFTW\")from the Julia REPL."
+    "text": "The package is available for Julia versions 0.6 and up. To install it, runPkg.add(\"FFTW\")from the Julia REPL.Users with a build of Julia based on Intel's Math Kernel Library (MKL) can take use MKL for FFTs by setting an environment variable JULIA_FFTW_PROVIDER to MKL and running Pkg.build(\"FFTW\"). Setting this environment variable only needs to be done for the first build of the package; after that, the package will remember to use MKL when building and updating. Note however that MKL provides only a subset of the functionality provided by FFTW. See Intel's documentation for more information about potential differences or gaps in functionality."
 },
 
 {
