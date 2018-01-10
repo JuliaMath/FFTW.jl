3fdb9260752727a19748bf060fe3ec26ecc0032d
Alex Arslan <ararslan@comcast.net>
2018-01-10 14:14:16 -0800

    Wrap adjoint calls in Matrix

--- src/fft.jl
+++ src/fft.jl
@@ -444,11 +444,11 @@ function dims_howmany(X::StridedArray, Y::StridedArray,
     end
     ist = [strides(X)...]
     ost = [strides(Y)...]
-    dims = adjoint([sz[reg] ist[reg] ost[reg]])
+    dims = Matrix(adjoint([sz[reg] ist[reg] ost[reg]]))
     oreg = [1:ndims(X);]
     oreg[reg] = 0
     oreg = filter(d -> d > 0, oreg)
-    howmany = adjoint([sz[oreg] ist[oreg] ost[oreg]])
+    howmany = Matrix(adjoint([sz[oreg] ist[oreg] ost[oreg]]))
     return (dims, howmany)
 end
 
