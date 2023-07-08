8da249560e266ba903f66a48efa0b008f8c104d1
Vedant Puri <vedantpuri@gmail.com>
2023-07-07 20:18:48 -0400

    rename args -> region

--- ext/FFTWChainRulesCoreExt.jl
+++ ext/FFTWChainRulesCoreExt.jl
@@ -6,26 +6,26 @@ using ChainRulesCore
 
 # DCT
 
-function ChainRulesCore.frule(Δ, ::typeof(dct), x::AbstractArray, args...)
+function ChainRulesCore.frule(Δ, ::typeof(dct), x::AbstractArray, region...)
     Δx = Δ[2]
-    y = dct(x, args...)
-    Δy = dct(Δx, args...)
+    y = dct(x, region...)
+    Δy = dct(Δx, region...)
     return y, Δy
 end
 
-function ChainRulesCore.rrule(::typeof(dct), x::AbstractArray, args...)
-    y = dct(x, args...)
+function ChainRulesCore.rrule(::typeof(dct), x::AbstractArray, region...)
+    y = dct(x, region...)
     project_x = ProjectTo(x)
 
     function dct_pullback(ȳ)
         f̄ = NoTangent()
-        x̄ = project_x(idct(unthunk(ȳ), args...))
-        ā = NoTangent()
+        x̄ = project_x(idct(unthunk(ȳ), region...))
+        r̄ = NoTangent()
 
-        if isempty(args)
+        if isempty(region)
             return f̄, x̄
         else
-            return f̄, x̄, ā
+            return f̄, x̄, r̄
         end
     end
 
@@ -34,26 +34,26 @@ end
 
 # IDCT
 
-function ChainRulesCore.frule(Δ, ::typeof(idct), x::AbstractArray, args...)
+function ChainRulesCore.frule(Δ, ::typeof(idct), x::AbstractArray, region...)
     Δx = Δ[2]
-    y = idct(x, args...)
-    Δy = idct(Δx, args...)
+    y = idct(x, region...)
+    Δy = idct(Δx, region...)
     return y, Δy
 end
 
-function ChainRulesCore.rrule(::typeof(idct), x::AbstractArray, args...)
-    y = idct(x, args...)
+function ChainRulesCore.rrule(::typeof(idct), x::AbstractArray, region...)
+    y = idct(x, region...)
     project_x = ProjectTo(x)
 
     function idct_pullback(ȳ)
         f̄ = NoTangent()
-        x̄ = project_x(dct(unthunk(ȳ), args...))
-        ā = NoTangent()
+        x̄ = project_x(dct(unthunk(ȳ), region...))
+        r̄ = NoTangent()
 
-        if isempty(args)
+        if isempty(region)
             return f̄, x̄
         else
-            return f̄, x̄, ā
+            return f̄, x̄, r̄
         end
     end
 
@@ -62,28 +62,32 @@ end
 
 # R2R
 
-function ChainRulesCore.frule(Δ, ::typeof(r2r), x::AbstractArray, args...)
+function ChainRulesCore.frule(Δ, ::typeof(r2r), x::AbstractArray, region...)
     Δx = Δ[2]
-    y = r2r(x, args...)
-    Δy = r2r(Δx, args...)
+    y = r2r(x, region...)
+    Δy = r2r(Δx, region...)
     return y, Δy
 end
 
-function ChainRulesCore.rrule(::typeof(r2r), x::AbstractArray, kinds, args...)
-    y = r2r(x, kinds, args...)
-    kinvs = Tuple(FFTW.inv_kind[k] for k in kinds)
+function ChainRulesCore.rrule(::typeof(r2r), x::AbstractArray, kinds, region...)
+    y = r2r(x, kinds, region...)
+    kinvs = if kinds isa Integer
+        FFTW.inv_kind[kinds]
+    else
+        Tuple(FFTW.inv_kind[k] for k in kinds)
+    end
     project_x = ProjectTo(x)
 
     function r2r_pullback(ȳ)
         f̄ = NoTangent()
-        x̄ = project_x(r2r(unthunk(ȳ), kinvs, args...))
+        x̄ = project_x(r2r(unthunk(ȳ), kinvs, region...))
         k̄ = NoTangent()
-        ā = NoTangent()
+        r̄ = NoTangent()
 
-        if isempty(args)
+        if isempty(region)
             return f̄, x̄, k̄
         else
-            return f̄, x̄, k̄, ā
+            return f̄, x̄, k̄, r̄
         end
     end
 
