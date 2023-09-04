38f72b491822a85bff67714d5871aeabcd878e23
Gaurav Arya <gauravarya272@gmail.com>
2023-09-03 22:04:41 -0400

    Use small p for consistency

--- src/fft.jl
+++ src/fft.jl
@@ -1054,4 +1054,4 @@ end
 
 AbstractFFTs.AdjointStyle(::cFFTWPlan) = AbstractFFTs.FFTAdjointStyle()
 AbstractFFTs.AdjointStyle(::rFFTWPlan{T, FORWARD}) where {T} = AbstractFFTs.RFFTAdjointStyle()
-AbstractFFTs.AdjointStyle(P::rFFTWPlan{T, BACKWARD}) where {T} = AbstractFFTs.IRFFTAdjointStyle(P.osz[first(P.region)])
+AbstractFFTs.AdjointStyle(p::rFFTWplan{T, BACKWARD}) where {T} = AbstractFFTs.IRFFTAdjointStyle(p.osz[first(p.region)])
