f6059272f6fcaa5311d523bf91ed3b4c7b8dbb43
Phillip Alday <phillip.alday@beacon.bio>
2024-03-25 11:33:04 -0500

    1.6 tweaks

--- ext/FFTWMKLExt.jl
+++ ext/FFTWMKLExt.jl
@@ -3,7 +3,7 @@ module FFTWMKLExt
 using FFTW
 
 # If we're using MKL, load it in and set library paths appropriately.
-@static if fftw_provider == "mkl"
+@static if FFTW.fftw_provider == "mkl"
     import MKL_jll
     FFTW.libfftw3[] = MKL_jll.libmkl_rt_path
     FFTW.libfftw3f[] = MKL_jll.libmkl_rt_path
@@ -13,7 +13,7 @@ function __init__()
     # Hook FFTW threads up to our partr runtime, and re-assign the
     # libfftw3{,f} refs at runtime, since we may have relocated and
     # changed the path to the library since the last time we precompiled.
-    @static if fftw_provider == "mkl"
+    @static if FFTW.fftw_provider == "mkl"
         FFTW.libfftw3[] = MKL_jll.libmkl_rt_path
         FFTW.libfftw3f[] = MKL_jll.libmkl_rt_path
     end
