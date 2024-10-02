b1eae21e376213cf2be978c721c7ef07da0c7c31
William S. Moses <gh@wsmoses.com>
2024-10-01 23:53:33 -0400

    fixup

--- ext/FFTWEnzymeExt.jl
+++ ext/FFTWEnzymeExt.jl
@@ -2,7 +2,7 @@ module FFTWEnzymeExt
 
 using Enzyme, FFTW
 
-Enzyme.EnzymeRules.inactive_noinl(typeof(FFTW.assert_applicable), x...) = true
+Enzyme.EnzymeRules.inactive_noinl(::typeof(FFTW.assert_applicable), x...) = true
 
 end # module
 
