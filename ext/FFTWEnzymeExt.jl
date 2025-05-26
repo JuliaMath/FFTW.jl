538a311ba634719b8d2e460074a1d52341464517
William S. Moses <gh@wsmoses.com>
2025-05-26 15:26:37 -0500

    mark time limit as inactive

--- ext/FFTWEnzymeExt.jl
+++ ext/FFTWEnzymeExt.jl
@@ -4,5 +4,7 @@ using Enzyme, FFTW
 
 Enzyme.EnzymeRules.inactive_noinl(::typeof(FFTW.assert_applicable), x...) = true
 
+Enzyme.EnzymeRules.inactive_noinl(::typeof(FFTW.unsafe_set_timelimit), x...) = true
+
 end # module
 
