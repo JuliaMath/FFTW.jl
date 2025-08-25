4401d483ca20465013a7f91881deaccf9838e95d
Jameson Nash <vtjnash@gmail.com>
2025-08-25 15:44:41 -0400

    Revert back to the standard form for ccall which now supports LazyLibrary improvements (#318)
    We had initially planned a breaking change design for LazyLibrary, which
    this package was updated for, but later we realized a better design
    which allows going back to the original version of this code. The
    previous form is still very poorly supported in the compiler and was
    still rather buggy internally.
    
    Revert "Replace `__init__` with `OncePerProcess` on supported Julia versions (#316)"
    This reverts commit dbcb5d210be95a0edac920646884784da87f7cab.
    
    Revert "Make FFTW relocatable for PackageCompiler"
    This reverts commit e96270a112ff2c16e19fc6aaac1ec429668ebe2f.
    
    Refs: [ccall: make distinction of pointer vs name a syntactic distinction](https://github.com/JuliaLang/julia/pull/59165)

--- src/FFTW.jl
+++ src/FFTW.jl
@@ -16,48 +16,67 @@ export dct, idct, dct!, idct!, plan_dct, plan_idct, plan_dct!, plan_idct!
 
 include("providers.jl")
 
-function initialize_library_paths()
+function fftw_init_check()
     # If someone is trying to set the provider via the old environment variable, warn them that they
     # should instead use `set_provider!()` instead.
     if haskey(ENV, "JULIA_FFTW_PROVIDER")
         Base.depwarn("JULIA_FFTW_PROVIDER is deprecated; use FFTW.set_provider!() instead", :JULIA_FFTW_PROVIDER)
     end
 
-    # Hook FFTW threads up to our partr runtime, and re-assign the
-    # libfftw3{,f} refs at runtime, since we may have relocated and
-    # changed the path to the library since the last time we precompiled.
+    # Hook FFTW threads up to our partr runtime
     @static if fftw_provider == "fftw"
-        libfftw3_path[] = FFTW_jll.libfftw3_path
-        libfftw3f_path[] = FFTW_jll.libfftw3f_path
         fftw_init_threads()
     end
-    @static if fftw_provider == "mkl"
-        libfftw3_path[] = MKL_jll.libmkl_rt_path
-        libfftw3f_path[] = MKL_jll.libmkl_rt_path
-    end
-    return nothing
 end
 
-if VERSION >= v"1.12.0-beta1.29"
-    const initialize_library_paths_once = OncePerProcess{Nothing}() do
-        initialize_library_paths()
-        return
-    end
-    function libfftw3()
-        initialize_library_paths_once()
-        return libfftw3_path[]
-    end
-    function libfftw3f()
-        initialize_library_paths_once()
-        return libfftw3f_path[]
-    end
-else
-    function __init__()
-        initialize_library_paths()
-    end
-    libfftw3() = libfftw3_path[]
-    libfftw3f() = libfftw3f_path[]
+if VERSION >= v"1.11.0"
+# This can be be deleted once FFTW_jll is upgraded to the real lazy jll code, to get the real benefits of this mess
+mutable struct FakeLazyLibrary
+    reallibrary::Symbol
+    on_load_callback
+    @atomic h::Ptr{Cvoid}
 end
+import Libdl: LazyLibrary, dlopen
+function dlopen(lib::FakeLazyLibrary)
+    h = @atomic :monotonic lib.h
+    h != C_NULL && return h
+    @lock fftwlock begin
+        h = @atomic :monotonic lib.h
+        h != C_NULL && return h
+        h = dlopen(getglobal(FFTW, lib.reallibrary))
+        lib.on_load_callback()
+        @atomic :release lib.h = h
+    end
+    return h
+end
+
+@static if fftw_provider == "fftw"
+    import FFTW_jll: libfftw3 as libfftw3_no_init,
+                     libfftw3f as libfftw3f_no_init
+elseif fftw_provider == "mkl"
+    import MKL_jll: libmkl_rt as libfftw3_no_init,
+                    libmkl_rt as libfftw3f_no_init
+end
+const libfftw3 = FakeLazyLibrary(:libfftw3_no_init, fftw_init_check, C_NULL)
+const libfftw3f = FakeLazyLibrary(:libfftw3f_no_init, fftw_init_check, C_NULL)
+
+else
+@static if fftw_provider == "fftw"
+    import FFTW_jll: libfftw3_path as libfftw3_no_init,
+                     libfftw3f_path as libfftw3f_no_init,
+                     libfftw3_path as libfftw3,
+                     libfftw3f_path as libfftw3f
+elseif fftw_provider == "mkl"
+    import MKL_jll: libmkl_rt_path as libfftw3_no_init,
+                    libmkl_rt_path as libfftw3f_no_init,
+                    libmkl_rt_path as libfftw3,
+                    libmkl_rt_path as libfftw3f
+end
+function __init__()
+    fftw_init_check()
+end
+end
+
 
 # most FFTW calls other than fftw_execute should be protected by a lock to be thread-safe
 const fftwlock = ReentrantLock()
